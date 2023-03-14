81ddaa1b7c76276dfffbed36edf61c2372b83cc0
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-03-14 16:43:59 +0400

    zedmeti faili

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import myPlugin from "../plugins/myPlugin";
 import axios from "axios";
 
 axios.defaults.baseURL=import.meta.env.VITE_BASE_API_URL
-axios.defaults.headers['Accept'] = 'Application/json'
 
   createApp(App)
     .use(store)
